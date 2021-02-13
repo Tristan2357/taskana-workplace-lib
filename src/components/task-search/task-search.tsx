ea9350a654c9e142da3bd7dbf74560d8e74bcdaf
Tristan Eisermann <tristan.eisermann@stud-mail.uni-wuerzburg.de>
2021-02-13 17:30:31 +0100

    --amend

--- src/components/task-search/task-search.tsx
+++ src/components/task-search/task-search.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, State } from '@stencil/core';
 
 @Component({
   tag: 'task-search',
@@ -6,6 +6,7 @@ import { Component, h } from '@stencil/core';
   shadow: true,
 })
 export class TaskSearch {
+  @State() searchBy: string = 'wb';
 
   render() {
     return <div class='flex'>
@@ -13,11 +14,24 @@ export class TaskSearch {
         <button class='btn'>
           <span class='material-icons caret'>keyboard_arrow_down</span>
         </button>
-        <input />
+        {this.searchBy == 'wb' ? <input type='text' placeholder='Search for Workbasket'/> : ''}
+
       </div>
-      <button class='btn right'>
-        <img src='../../assets/wb-empty.svg' width='20' height='20' alt='' />
-      </button>
+      <div class='dropdown'>
+        <button class='btn right'>{'Search by '}
+          <img src='../../assets/wb-empty.svg' width='20' height='20' alt='' />
+        </button>
+        <div class='dropdown-content'>
+          <div class='flex' onClick={() => this.searchBy = 'wb'}>
+            <img src='../../assets/wb-empty.svg' width='20' height='20' alt='' />
+            <span>Workbasket</span>
+          </div>
+          <div class='flex' onClick={() => this.searchBy = 'tv'}>
+            <span class='material-icons'><b>T</b></span>
+            <span>Type and Value</span>
+          </div>
+        </div>
+      </div>
     </div>;
   }
 }
